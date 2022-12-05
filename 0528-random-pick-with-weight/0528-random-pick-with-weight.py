64212fd96e38afe5cdb10b225cc4a941d79ef6e7
JinlongHuang <jlhuang42@gmail.com>
2022-12-05 13:24:42 -0800

    Time: 804 ms (19.40%), Space: 18.7 MB (23.74%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -3,22 +3,28 @@ from random import random
 class Solution:
     # F(x) ~ U(0,1)
     # random.random() ~ U(0,1)
-    # [1], [0.1, 0.9, 1]
+    # [1], [1/3, 1], [0.1, 0.9, 1]
 
     def __init__(self, w: List[int]):
-        total = sum(w)
+        self.total_sum = sum(w)
         prefix_sum = 0
         self.cum_w = []
         for ele in w:
             prefix_sum += ele
-            self.cum_w.append(prefix_sum / total)
+            self.cum_w.append(prefix_sum)
 
     def pickIndex(self) -> int:
-        r = random()
-        for i in range(len(self.cum_w)):
-            if r < self.cum_w[i]:
-                return i
-
+        target = random() * self.total_sum
+        n = len(self.cum_w)
+        left, right = 0, n - 1
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            if target > self.cum_w[mid]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
