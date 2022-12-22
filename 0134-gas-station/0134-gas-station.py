4841bf91fa10366aceeb15bf55def354ca95dc48
JinlongHuang <20373906+JinlongHuang@users.noreply.github.com>
2022-12-22 00:36:05 -0800

    Time: 671 ms (96.54%), Space: 24.2 MB (5.69%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        diff = [x - y for x, y in zip(gas, cost)]
+        prefix_sum = []
+        _sum = 0
+        for ele in diff:
+            _sum += ele
+            prefix_sum.append(_sum)
+        
+        print(prefix_sum)
+        if prefix_sum[-1] < 0:
+            return -1
+        else:
+            return (prefix_sum.index(min(prefix_sum)) + 1) % n
